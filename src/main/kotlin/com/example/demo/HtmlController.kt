7743fdc8a7ed1a2edf5a50d9d757eab8c7a31ac5
leyuri <leyuri97@gmail.com>
2020-09-21 16:08:34 +0900

    Add sign function & password encryption

--- src/main/kotlin/com/example/demo/HtmlController.kt
+++ src/main/kotlin/com/example/demo/HtmlController.kt
@@ -1,20 +1,30 @@
 package com.example.demo
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
+import java.lang.Exception
+import java.security.MessageDigest
 
 @Controller
 class HtmlController {
 
+    @Autowired
+    lateinit var repository:UserRepository // 나중에 초기화를 해준다?!
+
     @RequestMapping("/")
     fun index(model:Model) : String {
-        model.addAttribute("title","Home")
+        model.addAttribute("title","home")
         return "index"
     }
 
+    fun crypto(ss:String):String {
+        val sha=MessageDigest.getInstance("SHA-256")
+        val hexa = sha.digest(ss.toByteArray())
+        val crypto_str=hexa.fold("",{str, it -> str + "%02x".format(it)})
+        return crypto_str
+    }
     @GetMapping("/{formType}")
     fun htmlForm(model: Model, @PathVariable formType:String) : String {
 
@@ -30,5 +40,21 @@ class HtmlController {
         model.addAttribute("title", response)
 
         return response
+    }
+
+    @PostMapping("/sign")
+    fun postSign(model: Model,
+                 @RequestParam("id") userId:String,
+                 @RequestParam("password") password:String) : String {
+
+        try {
+            val cryptoPass=crypto(password)
+
+            repository.save(User(userId, cryptoPass))
+        } catch (e:Exception) {
+            e.printStackTrace()
+        }
+        model.addAttribute("title", "sign sucess")
+        return "login"
     }
 }
