7809be1653dc6f00f006674c00d5bb8533770ce2
leyuri <leyuri97@gmail.com>
2020-09-21 14:30:16 +0900

    Add sign page

--- src/main/kotlin/com/example/demo/HtmlController.kt
+++ src/main/kotlin/com/example/demo/HtmlController.kt
@@ -2,6 +2,8 @@ package com.example.demo
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
@@ -10,5 +12,10 @@ class HtmlController {
     @RequestMapping("/")
     fun index(model:Model) : String {
         return "index"
+    }
+
+    @GetMapping("/sign")
+    fun htmlForm(model: Model) : String {
+        return "sign"
     }
 }
