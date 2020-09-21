5d25364cbae34282595ee82ec524e3d0adf36d23
leyuri <leyuri97@gmail.com>
2020-09-21 13:52:09 +0900

    Add mustache templates setting

--- src/main/kotlin/com/example/demo/HtmlController.kt
+++ src/main/kotlin/com/example/demo/HtmlController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 class HtmlController {
 
     @RequestMapping("/")
-    fun index(model : Model) {
-        println("index method")
+    fun index(model:Model) : String {
+        return "index"
     }
 }
