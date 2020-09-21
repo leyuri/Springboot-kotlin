7ef7f2b236657d019c69d235176e188c700bb088
leyuri <leyuri97@gmail.com>
2020-09-21 13:40:36 +0900

    first commit

---
+++ src/main/kotlin/com/example/demo/HtmlController.kt
@@ -0,0 +1,14 @@
+package com.example.demo
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+class HtmlController {
+
+    @RequestMapping("/")
+    fun index(model : Model) {
+        println("index method")
+    }
+}
